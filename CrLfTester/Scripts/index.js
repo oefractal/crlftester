eff51a4ef88a6aba08b8c1b2994165f0239898b0
oefractal <oefractal@gmail.com>
2018-08-06 21:06:22 +0400

    Обновление грида после изменения символа перевода строки

--- CrLfTester/Scripts/index.js
+++ CrLfTester/Scripts/index.js
@@ -1,8 +1,12 @@
-﻿$(function () {
+﻿function ReloadResultsGrid() {
+  var url = $("#resultsData").data("request-url");
+  var encodedFolderName = encodeURIComponent($("#folderInput").val());
+  var encodedFilter = encodeURIComponent($("#fileFilter").val());
+  $("#resultsGrid").load(url + "?folderName=" + encodedFolderName + "&filter=" + encodedFilter);
+}
+
+$(function () {
   $("#testButton").on("click", function () {
-    var url = $("#resultsData").data("request-url");
-    var encodedFolderName = encodeURIComponent($("#folderInput").val());
-    var encodedFilter = encodeURIComponent($("#fileFilter").val());
-    $("#resultsGrid").load(url + "?folderName=" + encodedFolderName + "&filter=" + encodedFilter);
+    ReloadResultsGrid();
   });
 });
